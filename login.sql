abb893cbc1efeeeb31ac897b424c3273079e5564
Jose Julián Montoya Díaz <107949450+JotaDiaz7@users.noreply.github.com>
2023-06-14 23:05:23 +0200

    Add files via upload

---
+++ login.sql
@@ -0,0 +1,47 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         8.0.30 - MySQL Community Server - GPL
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Volcando estructura para tabla login.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `pass` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- Volcando datos para la tabla login.users: ~12 rows (aproximadamente)
+DELETE FROM `users`;
+INSERT INTO `users` (`email`, `name`, `pass`) VALUES
+	('carol@hola.es', 'carol', '22ac3c5a5bf0b520d281c122d1490650'),
+	('hola@mundo.com', 'jota diaz', '2e65f2f2fdaf6c699b223c61b1b5ab89'),
+	('hola@mundo.es', 'John Doe', '81dc9bdb52d04dc20036dbd8313ed055'),
+	('jota@hola.es', 'Jota Díaz', '2e65f2f2fdaf6c699b223c61b1b5ab89'),
+	('mari@hola.es', 'mari', '2e65f2f2fdaf6c699b223c61b1b5ab89'),
+	('mari@mari.es', 'mari', '2e65f2f2fdaf6c699b223c61b1b5ab89'),
+	('nena@hola.es', 'NENA', '1ce927f875864094e3906a4a0b5ece68'),
+	('paco@hola.es', 'paco', '250cf8b51c773f3f8dc8b4be867a9a02'),
+	('paula@hola.com', 'Paula', '68053af2923e00204c3ca7c6a3150cf7'),
+	('paula@hola.es', 'Paula', '68053af2923e00204c3ca7c6a3150cf7'),
+	('pepe@hola.es', 'pepe', '2e65f2f2fdaf6c699b223c61b1b5ab89'),
+	('pepe@pepe.com', 'pepe', '2e65f2f2fdaf6c699b223c61b1b5ab89'),
+	('toni@hola.es', 'toni', '2e65f2f2fdaf6c699b223c61b1b5ab89');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
